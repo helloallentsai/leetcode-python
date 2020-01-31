16e33548cbe02a77d34c9b09a4cc7629b3291e44
Allen Tsai <hello.allen.tsai@gmail.com>
2020-01-30 23:00:54 -0800

    completed 1281

---
+++ 1281. Subtract the Product and Sum of Digits of an Integer.py
@@ -0,0 +1,40 @@
+# 1281. Subtract the Product and Sum of Digits of an Integer
+# Easy
+
+# 101
+
+# 55
+
+# Add to List
+
+# Share
+# Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+
+# Example 1:
+
+# Input: n = 234
+# Output: 15
+# Explanation:
+# Product of digits = 2 * 3 * 4 = 24
+# Sum of digits = 2 + 3 + 4 = 9
+# Result = 24 - 9 = 15
+# Example 2:
+
+# Input: n = 4421
+# Output: 21
+# Explanation:
+# Product of digits = 4 * 4 * 2 * 1 = 32
+# Sum of digits = 4 + 4 + 2 + 1 = 11
+# Result = 32 - 11 = 21
+
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        nums = str(n)
+        prod = 1
+        sum = 0
+        for digit in nums:
+            digit = int(digit)
+            prod *= digit
+            sum += digit
+        return prod-sum
